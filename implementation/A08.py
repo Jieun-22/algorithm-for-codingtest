9619edf037d0fe79a41eb3708f1f8248fe703bb4
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2021-12-22 22:36:12 +0900

    Create A08.py
    알파벳인지 아닌지 확인 data.isalpha() -> 알파벳이면 True 출력
    
    리스트를 문자열로 변환하여 출력 print(''.join(result))

---
+++ implementation/A08.py
@@ -0,0 +1,19 @@
+# 문자열 재정렬
+
+data = input()
+
+result = []
+sum = 0
+
+for i in data:
+  if i.isalpha():
+    result.append(i)
+  else:
+    sum += int(i)
+
+result.sort()
+
+if sum != 0:
+  result.append(str(sum))
+
+print(''.join(result))
