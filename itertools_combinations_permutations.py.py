016d3c10f5d922172246aa74b1b068ff32bd7ba7
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2021-12-27 18:14:32 +0900

    Create itertools_combinations_permutations.py.py

---
+++ itertools_combinations_permutations.py.py
@@ -0,0 +1,10 @@
+from itertools import combinations 
+from itertools import permutations
+
+candidates = list(combinations(data, m))
+# 조합으로 m 개 뽑는 경우 리스트
+                  
+candidates = list(permutations(data,m))
+# 순열 m 개 뽑는 경우 리스트
+                  
+      
