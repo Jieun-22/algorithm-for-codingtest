5e2d035a92d017d9d57c66f59298d30bc5e3cb07
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-01-20 17:57:56 +0900

    Update A28.py

--- BinarySearch/A28.py
+++ BinarySearch/A28.py
@@ -7,11 +7,10 @@ def binary_search(array, start, end):
 
   while start <= end:
     mid = (start+end)//2
-    target = mid
 
-    if array[mid] == target:
+    if array[mid] == mid:
       return mid
-    elif array[mid] < target:
+    elif array[mid] < mid:
       start = mid +1
     else:
       end = mid -1
