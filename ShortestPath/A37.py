4abfc08b5293731e805af24228346bbaf62329af
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-01-29 00:29:57 +0900

    Create A37.py

---
+++ ShortestPath/A37.py
@@ -0,0 +1,34 @@
+INF = int(1e9)
+
+# 도시의 개수 
+n = int(input())
+#버스의 개수
+m = int(input())
+
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    if i == j:
+      graph[i][j] = 0
+
+
+for _ in range(m):
+  a, b, c = map(int, input().split())
+  if c < graph[a][b]:
+    graph[a][b] = c
+
+for k in range(1, n+1):
+  for i in range(1, n+1):
+    for j in range(1, n+1):
+      graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    if graph[i][j] != INF:
+      print(graph[i][j], end = ' ')
+    else:
+      print(0)
+  print()
+
+
