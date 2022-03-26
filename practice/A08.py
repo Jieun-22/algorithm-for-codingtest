fa43080de1eaba26fa4693f1ef2d0be8cca9cae1
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-03-26 18:11:26 +0900

    Create A08.py

---
+++ practice/A08.py
@@ -0,0 +1,18 @@
+#문자열 재정렬
+
+data = input()
+result = []
+sum = 0
+
+for x in data:
+  if x.isalpha() :
+    result.append(x)
+  else:
+    sum += int(x)
+
+result.sort()
+
+if sum != 0:
+  result.append(str(sum))
+
+print(''.join(result))
