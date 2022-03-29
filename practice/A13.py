27e2470e694af4bdbf3f8708d9ef4c272b6ee0bb
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-03-29 21:26:34 +0900

    Create A13.py

---
+++ practice/A13.py
@@ -0,0 +1,34 @@
+# 치킨배달
+
+from itertools import combinations
+
+n, m = map(int, input().split())
+
+home = []
+chicken = []
+
+for x in range(n):
+  data = list(map(int, input().split()))
+  for y in range(n):
+    if data[y] == 1:
+      home.append((x,y))
+    elif data[y] == 2:
+      chicken.append((x,y))
+
+candidates = list(combinations(chicken, m))
+
+def get_sum(candidate):
+  result = 0
+  for hx, hy in home:
+    temp = 1e9
+    for cx,cy in candidate:
+      temp = min(temp, abs(hx-cx) + abs(hy-cy))
+
+    result += temp
+  return result
+
+result = 1e9
+for candidate in candidates:
+  result = min(result, get_sum(candidate))
+
+print(result)
