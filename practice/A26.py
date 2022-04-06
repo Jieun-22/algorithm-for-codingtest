bad1528d09b2a345d7100283929e98f64d499694
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-06 22:54:43 +0900

    Create A26.py

---
+++ practice/A26.py
@@ -0,0 +1,20 @@
+#카드 정렬하기
+
+n = int(input())
+
+data = []
+
+for _ in range(n):
+  data.append(int(input()))
+
+data.sort()
+result = []
+
+summary = data[0]
+
+for i in range(1,n):
+  summary = summary + data[i]
+  result.append(summary)
+
+print(sum(result))
+
