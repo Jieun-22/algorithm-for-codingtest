44b22b3a27a0edb290d91572d6c23bc9a361c403
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-18 00:01:05 +0900

    Create A28.py

---
+++ practice/A28.py
@@ -0,0 +1,22 @@
+# 고정점 찾기
+
+n = int(input())
+array = list(map(int, input().split()))
+
+start = 0
+end = len(array) -1
+
+result = -1
+
+while start <= end:
+  mid = (start+end) //2
+  if array[mid] == mid :
+    result = mid
+    break
+  elif array[mid] > mid :
+    end = mid - 1
+  else:
+    start = mid + 1
+
+print(result)
+    
