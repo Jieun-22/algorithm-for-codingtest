f69973ce33a988602097639a765da35a648b4052
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-25 02:22:35 +0900

    Update A25.py

--- practice/A25.py
+++ practice/A25.py
@@ -1,22 +1,26 @@
 def solution(N, stages):
     
     answer = []
+    
+    data = [0]*(N+2)
+    
+    for x in stages:
+        data[x] += 1
+    
     length = len(stages)
     
-    for i in range(1, N+1):
-        count = stages.count(i)
+    for i in range(1,N+1):
+        if length != 0:
+            answer.append((data[i]/length,i))
+            length -= data[i]
+        elif length == 0:
+            answer.append((0,i))
+      
+    answer.sort(key = lambda x: (-x[0], x[1]))
+    for i in range(len(answer)):
+        answer[i] = answer[i][1]
         
-        if length == 0:
-            fail = 0
-        else:
-            fail = count/ length
         
-        answer.append((i,fail))
-        length -= count
-        
-    answer = sorted(answer, key = lambda t:t[1], reverse = True)
-    
-    answer = [i[0] for i in answer]
-    
     return answer
+
 
