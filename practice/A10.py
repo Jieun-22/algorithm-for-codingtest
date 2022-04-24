44b45bd9a577a22c8caba6cc000bf66e312b758d
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-24 16:49:12 +0900

    Update A10.py

--- practice/A10.py
+++ practice/A10.py
@@ -1,8 +1,7 @@
-def rotate_90_degree(a):
+def rotate_a_matrix_by_90_degree(a):
     n = len(a)
     m = len(a[0])
     result = [[0]*n for _ in range(m)]
-    
     for i in range(n):
         for j in range(m):
             result[j][n-i-1] = a[i][j]
@@ -10,25 +9,25 @@ def rotate_90_degree(a):
     return result
 
 def check(new_lock):
-    length = len(new_lock)//3
-    for i in range(length ,2*length):
-        for j in range(length,2*length):
+    lock_length = len(new_lock)// 3
+    for i in range(lock_length, lock_length*2):
+        for j in range(lock_length, lock_length*2):
             if new_lock[i][j] != 1:
                 return False
+            
     return True
-    
+
 def solution(key, lock):
-    
     n = len(lock)
     m = len(key)
     
-    new_lock = [[0]*(n*3) for _ in range((n*3))]
+    new_lock = [[0]*(n*3) for _ in range(n*3)]
     for i in range(n):
         for j in range(n):
             new_lock[i+n][j+n] = lock[i][j]
-    
+            
     for rotation in range(4):
-        key = rotate_90_degree(key)
+        key = rotate_a_matrix_by_90_degree(key)
         for x in range(n*2):
             for y in range(n*2):
                 for i in range(m):
@@ -40,5 +39,3 @@ def solution(key, lock):
                     for j in range(m):
                         new_lock[x+i][y+j] -= key[i][j]
     return False
-            
-    
