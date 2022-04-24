213e2b85b427674526b5a74cea5b2e78e776c582
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-24 16:48:31 +0900

    Update A11.py

--- practice/A11.py
+++ practice/A11.py
@@ -1,66 +1,56 @@
-#뱀 A11
-
+# 11 뱀
 
 n = int(input())
-k = int(input())
-
 data = [[0]*(n+1) for _ in range(n+1)]
 
+k = int(input())
 for _ in range(k):
   x, y = map(int, input().split())
-  data[x][y] = 1 
+  data[x][y] = 1
 
 l = int(input())
-
-info = []
+info = [0] * (10001)
 
 for _ in range(l):
-  a, b = input().split()
-  info.append((int(a), b))
+  x, c = input().split()
+  info[int(x)] = c
 
 dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+dy = [1,0,-1,0]
 
-def turn(direction, c):
+def turn_direction(direction, c):
   if c == 'L':
-    direction = (direction-1) % 4
+    direction = (direction-1)%4
   else:
     direction = (direction+1) % 4
-
+  
   return direction
 
-def simulate():
-  x, y = 1, 1
-  data[x][y] = 2
-  direction = 0
-  time = 0
-  index = 0
-  q = [(x,y)]
+x,y = 1,1
+data[x][y]  = 2
+q = [(x,y)]
+t = 0
+direction = 0
 
-  while True:
-    nx = x + dx[direction]
-    ny = y + dy[direction]
+while True:
+  nx = x + dx[direction]
+  ny = y + dy[direction]
 
-    if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2:
-      if data[nx][ny] == 0:
-        data[nx][ny] = 2
-        q.append((nx, ny))
-        px, py = q.pop(0)
-        data[px][py] = 0
-      if data[nx][ny] == 1 :
-        data[nx][ny] == 2
-        q.append((nx,ny))
-    
-    else:
-      time += 1
-      break
-    
-    x,y = nx, ny
-    time += 1
-    if index < l and time == info[index][0]:
-      direction = turn(direction, info[index][1])
-      index += 1
-  
-  return time
+  if 1 <= nx and nx <= n and 1 <= ny and ny <= n and data[nx][ny] != 2 :
+    if data[nx][ny] == 0: # 아무것도 없다면 
+      data[nx][ny] = 2
+      q.append((nx,ny))
+      rx, ry = q.pop(0)
+      data[rx][ry] = 0
+    elif data[nx][nx] == 1 : #사과 있다면
+      data[nx][ny] = 2
+      q.apped((nx,ny))
+  else:
+    t += 1
+    break
+  x,y = nx, ny
+  t += 1
+  if info[t] != 0:
+    direction = turn_direction(direction, info[t])
 
-print(simulate())
+print(t)
