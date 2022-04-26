4975ca3bc7a2ba45588cb3433033f2e60836de68
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-27 00:05:05 +0900

    Update A32.py

--- practice/A32.py
+++ practice/A32.py
@@ -2,23 +2,20 @@
 
 n = int(input())
 
-dp = []
-
+data = []
 for _ in range(n):
-  dp.append(list(map(int, input().split())))
+  data.append(list(map(int, input().split())))
 
 for i in range(1,n):
   for j in range(i+1):
     if j == 0:
-      up_left = 0
-    else: up_left = dp[i-1][j-1]
-
-    if j == i:
-      up = 0
-    else: up = dp[i-1][j]
-
-    dp[i][j] = dp[i][j] + max(up_left, up)
+      data[i][j] += data[i-1][j]
+    elif j == i:
+      data[i][j] += data[i-1][j-1]
+    else:
+      data[i][j] += max(data[i-1][j-1], data[i-1][j])
 
 
-print(max(dp[n-1]))
+print(max(data[n-1]))
+
 
