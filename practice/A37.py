965064f0987b30e21756ab4d2a6fa6bce8a9f1f3
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-27 01:33:10 +0900

    Update A37.py

--- practice/A37.py
+++ practice/A37.py
@@ -12,8 +12,7 @@ for i in range(1,n+1):
 
 for _ in range(m):
   a,b,c = map(int, input().split())
-  if c < graph[a][b] :
-    graph[a][b] = c
+  graph[a][b] = min(graph[a][b], c)
 
 for k in range(1,n+1):
   for a in range(1, n+1):
