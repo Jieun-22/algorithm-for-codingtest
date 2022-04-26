b867f4b3c22fdaf69b199c2ff71ed64dbed0789e
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-26 16:55:11 +0900

    Update A27.py

--- practice/A27.py
+++ practice/A27.py
@@ -1,18 +1,14 @@
-#정렬된 배열에서 특정수의 개수 구하기
-
+# 27
 from bisect import bisect_left, bisect_right
 
 n, x = map(int, input().split())
 data = list(map(int, input().split()))
 
-first = bisect_left(data, x)
-last = bisect_right(data, x)
+left = bisect_left(data,x)
+right = bisect_right(data,x)
 
-result = last - first
-
-if result == 0:
+if left == right:
   print(-1)
 else:
-  print(result)
-
+  print(right - left)
 
