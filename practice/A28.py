4e9fc4212b6685999e9a36f314e3686b03c21e0c
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-26 17:07:46 +0900

    Update A28.py

--- practice/A28.py
+++ practice/A28.py
@@ -1,22 +1,21 @@
-# 고정점 찾기
+# 28 고정점 찾기
 
 n = int(input())
 array = list(map(int, input().split()))
 
 start = 0
 end = len(array) -1
-
 result = -1
 
 while start <= end:
-  mid = (start+end) //2
-  if array[mid] == mid :
+  mid = (start+end)//2
+  if array[mid] == mid:
     result = mid
     break
-  elif array[mid] > mid :
-    end = mid - 1
-  else:
+  elif array[mid] > mid:
+    end = mid -1
+  else: 
     start = mid + 1
 
 print(result)
-    
+
