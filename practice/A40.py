9ca74d9971d35d4dac2f7e6e809048b0cbe04ff7
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-27 16:29:47 +0900

    Update A40.py

--- practice/A40.py
+++ practice/A40.py
@@ -1,25 +1,25 @@
-# 숨바꼭질
-
+#40 숨바꼭질
 import heapq
+
+n, m = map(int, input().split())
 
 INF = int(1e9)
 
-n,m = map(int, input().split())
-
-graph = [[] for _ in range(n+1)]
-distance = [INF]*(n+1)
+graph= [[]for _ in range(n+1)]
+distance = [INF] * (n+1)
 
 for _ in range(m):
   a,b = map(int, input().split())
   graph[a].append((b,1))
   graph[b].append((a,1))
 
-q = []
-heapq.heappush(q,(0,1))
+q= []
 distance[1] = 0
+heapq.heappush(q,(0,1))
+
 while q:
   dist, now = heapq.heappop(q)
-  if distance[now] < dist :
+  if distance[now] < dist:
     continue
   for i in graph[now]:
     cost = dist + i[1]
@@ -28,20 +28,20 @@ while q:
       heapq.heappush(q,(cost, i[0]))
 
 num = 0
-max_cost = 0
-count = 0
+dis = 0
+cnt = 0
 
-for i in range(2,n+1):
+for i in range(1,n+1):
   if distance[i] == INF:
     continue
-  if distance[i] > max_cost :
+  if distance[i] > dis:
     num = i
-    max_cost = distance[i]
-    count = 1
-  elif distance[i] == max_cost :
-    count += 1
+    dis = distance[i]
+    cnt = 1
+  elif distance[i] == dis:
+    cnt += 1
 
+print(num, dis, cnt)
 
-print(num, max_cost, count)
 
 
