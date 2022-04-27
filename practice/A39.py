a2cff615a305ba2037040fdb3ed2e333ec9c1e86
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-27 16:53:37 +0900

    Update A39.py

--- practice/A39.py
+++ practice/A39.py
@@ -1,41 +1,39 @@
-# 화성탐사
-
+# 39 화성 탐사
 import heapq
 
 INF = int(1e9)
 
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
+t = int(input())
 
-for tc in range(int(input())):
+for _ in range(t):
   n = int(input())
 
   graph = []
-  for i in range(n):
+  for _ in range(n):
     graph.append(list(map(int, input().split())))
 
-  distance = [[INF]* n for _ in range(n)]
+  result= [[INF]*n for _ in range(n)]
 
-  x, y = 0,0
-  q = [(graph[x][y],x,y)] 
-  distance[x][y] = graph[x][y]
+  dx = [0,1,0,-1]
+  dy = [1,0,-1,0]
+  
+  result[0][0] = graph[0][0]
+  q = []
+  heapq.heappush(q,(result[0][0],0,0))
 
   while q:
     dist, x, y = heapq.heappop(q)
-    if distance[x][y] < dist:
+    if result[x][y] < dist:
       continue
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
 
-      if nx <0 or nx >= n or ny < 0 or ny >=n:
-        continue
-      cost = dist + graph[nx][ny]
-      if cost < distance[nx][ny]:
-        distance[nx][ny] = cost
-        heapq.heappush(q,(cost,nx,ny))
-    
+      if nx >= 0 and nx <n and ny >= 0 and ny <n:
+        cost = dist + graph[nx][ny]
+        if cost < result[nx][ny]:
+          result[nx][ny] = cost
+          heapq.heappush(q,(cost, nx,ny))
+          
+  print(result[n-1][n-1])
   
-  print(distance[n-1][n-1])
-
-
