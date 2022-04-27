a0749bd5148d1b08a9ccfa2e1f209568cc0ec152
Lee JiEun <95852422+Jieun-22@users.noreply.github.com>
2022-04-27 21:19:13 +0900

    Update A44.py

--- practice/A44.py
+++ practice/A44.py
@@ -1,50 +1,32 @@
 # 행성 터널
-from itertools import combinations
-
-def find_parent(parent, x):
-  if parent[x] != x:
-    parent[x] = find_parent(parent,parent[x])
-
-  return parent[x]
-
-def union_parent(parent, a, b):
-  a = find_parent(parent, a)
-  b = find_parent(parent, b)
-
-  if a <b:
-    parent[b] = a
-  else:
-    parent[a] = b
 
 n = int(input())
-loc = []
+
+data = []
 
 for _ in range(n):
-  loc.append(list(map(int, input().split())))
-
-array = [i for i in range(n)]
-
-candidates = list(combinations(array,2))
+  x,y,z = map(int, input().split())
+  data.append((x,y,z))
 
 edges = []
-parent = [0]*(n)
+
+for i in range(n):
+  for j in range(i+1,n):
+    cost = min(abs(data[i][0]- data[j][0]), abs(data[i][1]- data[j][1]), abs(data[i][2]- data[j][2]))
+    edges.append((cost,i,j))
+
+edges.sort()
+parent = [0]*n
 
 for i in range(n):
   parent[i] = i
 
-for candidate in candidates:
-  a,b = candidate
-  cost = min(abs(loc[a][0]-loc[b][0]), abs(loc[a][1]-loc[b][1]), abs(loc[a][2]- loc[b][2]))
-  edges.append((cost,a,b))
-
-edges.sort()
-
 result = 0
 
 for edge in edges:
-  cost, a, b = edge
-  if find_parent(parent,a) != find_parent(parent,b):
-    union_parent(parent,a,b)
+  cost,i,j = edge
+  if find_parent(parent,i) != find_parent(parent,j):
+    union_parent(parent,i,j)
     result += cost
   
 print(result)
